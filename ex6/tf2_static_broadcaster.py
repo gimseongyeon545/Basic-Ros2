6230ee4186cca8b94e5518d2244a23873b9d0520
gimseongyeon545 <gimseongyeon545@gmail.com>
2025-09-28 20:35:52 +0900

    Update tf2_static_broadcaster.py

--- ex6/tf2_static_broadcaster.py
+++ ex6/tf2_static_broadcaster.py
@@ -1,15 +1,15 @@
 # 06_tf2_static_broadcaster.py
 import rclpy
 from rclpy.node import Node
-from tf2_ros import StaticTransformBroadcaster
 from geometry_msgs.msg import TransformStamped
+from tf2_ros import StaticTransformBroadcaster
 import math
 
 class StaticTF(Node):
     def __init__(self):
         super().__init__('static_tf')
         self.br = StaticTransformBroadcaster(self)
-      
+        
         t = TransformStamped()
         t.header.frame_id = 'base_link'
         t.child_frame_id  = 'camera_link'
@@ -21,8 +21,9 @@ class StaticTF(Node):
         
         import tf_transformations as tft
         q = tft.quaternion_from_euler(0.0, math.radians(90), 0.0)
+        
         t.transform.rotation.x, t.transform.rotation.y, t.transform.rotation.z, t.transform.rotation.w = q
-      
+        
         self.br.sendTransform(t)
         self.get_logger().info('static TF published')
 
