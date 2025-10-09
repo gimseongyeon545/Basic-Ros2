7379601d2dee4fe0453132e36b78d0aed32d1f79
gimseongyeon545 <gimseongyeon545@gmail.com>
2025-10-09 21:47:09 +0900

    Update 1.py

--- practice/p1/1.py
+++ practice/p1/1.py
@@ -1,1 +1,21 @@
-e
+# [heartbeat_pub] ──(String /mini/heartbeat)──▶ [status_sub]
+# [heartbeat_pub]
+
+import rclpy.node as Node
+import rclpy
+
+class HeartBeatpub(Node):
+  def __init__(self):
+    super().__init__('heartbeat_pub')
+    self.HBpub = self.create_publisher(String, '/mini/heartbeat', 10)
+    self.timer = self.create_timer(0.5, self.cb)
+    self.cnt = 0
+    
+  def cb(self, msg):
+    self.cnt += 1
+    self.HBpub.publish(msg)
+    self.get_logger().info(f'heartbeat pub cnt: {cnt}')
+
+rclpy.init()
+rclpy.spin(HeartBeatpub)
+rclpy.shutdown()
