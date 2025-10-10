848066466cc74412c1bee97e5f80d11c11660279
gimseongyeon545 <gimseongyeon545@gmail.com>
2025-10-10 21:16:40 +0900

    Update heartbeat_pub.py

--- practice/p1/heartbeat_pub.py
+++ practice/p1/heartbeat_pub.py
@@ -1,8 +1,10 @@
-'''
+"""
 [heartbeat_pub] --( /mini/heartbeat : std_msgs/String )--> [status_sub]
         │                                                   ▲
         └─ uses Timer(hz) & Parameters(text,hz) ────────────┘
-'''
+        
+[ros2 param]* ──(set/get)──> [heartbeat_pub]   *CLI로 hz,text 동적 변경
+"""
 
 from rclpy.node import Node
 import rclpy
@@ -15,11 +17,15 @@ class HeartBeatpub(Node):
     self.HBpub = self.create_publisher(String, '/mini/heartbeat', 10)
     self.timer = self.create_timer(0.5, self.cb)
     self.cnt = 0
-    
+    self.declare_paramter('text', 'hi')
+    self.declare_paramter('hz', 2.0)
+    self.hz = self.get_paramter('hz')
+        
   def cb(self):
     self.cnt += 1
     msg = String()
-    msg.data = 'heartbeat msg'
+    txt = self.get_paramter('text')
+    msg.data = txt
     self.HBpub.publish(msg)
     self.get_logger().info(f'heartbeat pub cnt: {self.cnt}')  
 
