8178e7f727736d3c01ab3e10eb78b4c34a940d44
gimseongyeon545 <gimseongyeon545@gmail.com>
2025-10-11 21:01:30 +0900

    Update gripper_action_client.py

--- practice/p3/gripper_action_client.py
+++ practice/p3/gripper_action_client.py
@@ -1,3 +1,10 @@
+'''
+[gripper_action_client] ==goal==> (GripperCommand.action server)
+       ▲                 <== feedback/result ==
+       └==================== cancel =====================
+'''
+
+
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient # action client = Goal, Feedback, Result(cancel) / service client = request, response
@@ -22,7 +29,8 @@ class gripper_action_client(Node):
     self.goal.command.effort = 40.0
 
     # [2] send_goal_async(): Goal 전달 -> future 반환
-    self.goal_future = self.ac.send_goal_async(self.goal) # future class (Future[ClientGoalHandle])
+    # feedback 함수: goal 보내고 server 가 feedback 보낼 때 호출됨
+    self.goal_future = self.ac.send_goal_async(self.goal, feedback_callback = self.feedback) # future class (Future[ClientGoalHandle])
 
     # [3] spin_until_future_complete() -> future.result() + accepted
     rclpy.spin_until_future_complete(self.goal_future)
@@ -33,6 +41,21 @@ class gripper_action_client(Node):
     else:
       self.get_logger().warn('goal not accepted')
 
+    #========[Cancel]========= 
+    # 실행 중인 goal cancel
+    # [1] cancel_goal_async
+    self.cancel_future = self.goal_handle.cancel_goal_async()
+
+    # [2] spin_until_future_complete() -> future.result()
+    rclpy.spin_until_future_complete(self.cancel_future)
+    self.cancel_res = self.cancel_future.result()
+
+    # [3] .return_code == 0(Error), 1(Rejected), 2(Accepted)
+    if self.cancel_res.return_code == 2:
+      self.get_logger().info('cancel accepted')
+    else:
+      self.get_logger().warn(f'cancel not accepted: {self.cancel_res.return_code}')
+    
     
     #========[Result]========= 
     # [1] get_result_async()
@@ -47,5 +70,9 @@ class gripper_action_client(Node):
     self.res_status = self.res.status
     self.res_result = self.res.result
 
-
+#========[Feedback]========= 
+# 중간마다 보내는 상태 업데이트 / goal accepted 후 최종 result 나오기 전까지 필요할 때마다 도착
+def feedback(self, fb_msg):
+  self.fb = fb_msg.feedback
+  self.get_logger().info(f'Feedback: pos = {self.fb.position}, effort = {self.fb.effort}')
     
