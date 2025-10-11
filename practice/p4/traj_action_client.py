91ff9b432675060d0e01b13a7438e4d25adc325f
gimseongyeon545 <gimseongyeon545@gmail.com>
2025-10-11 21:06:32 +0900

    Create traj_action_client.py

---
+++ practice/p4/traj_action_client.py
@@ -0,0 +1,5 @@
+'''
+[traj_action_client] ==goal(JointTrajectory)==> (FollowJointTrajectory server)
+        ▲                     <== feedback/result ==
+        └========================= cancel =========================
+'''
